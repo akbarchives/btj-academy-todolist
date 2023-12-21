18b72e51d5f2f9363b48258c2eaa08f1862d7c07
akbarchives <rizqiakbar2412@gmail.com>
2023-12-21 08:44:38 +0700

    fix when list empty

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -43,7 +43,7 @@
           id="name"
           v-model="newTodo.name"
           required
-          placeholder="add task name"
+          placeholder="What needs to be done?"
         />
       </div>
       <div class="flex grow flex-col">
@@ -71,14 +71,19 @@
     <!-- To Do List -->
     <div class="container flex flex-col gap-4 sm:flex-row">
       <div class="w-full sm:w-1/2">
-        <h1 class="my-4 text-3xl font-medium">Todo</h1>
+        <h1
+          class="my-4 border border-transparent border-b-gray-200 py-2 text-3xl font-medium"
+        >
+          ToDo
+        </h1>
         <div
+          v-if="checkToDoList"
           v-for="(list, index) in toDoList"
           :key="index"
-          class="my-2 flex flex-col-reverse gap-2 rounded-lg border p-2"
+          class="my-2 flex flex-col-reverse gap-2 rounded-lg border"
         >
-          <div class="group flex flex-col gap-2">
-            <p class="font-medium">{{ list.name }}</p>
+          <div class="group flex flex-col gap-2 p-2 hover:bg-slate-100">
+            <p class="text-2xl font-medium sm:text-base">{{ list.name }}</p>
             <div class="flex justify-between text-sm">
               <p class="flex items-center gap-1">
                 <span
@@ -91,13 +96,13 @@
               <div class="flex gap-2">
                 <button
                   @click="deleteToDoList(index)"
-                  class="invisible rounded-md bg-red-500 px-2 py-1 font-thin text-white duration-200 ease-in-out hover:bg-red-600 group-hover:visible"
+                  class="invisible rounded-md bg-red-500 px-2 py-1 font-medium text-white duration-200 ease-in-out hover:bg-red-600 group-hover:visible"
                 >
                   Delete
                 </button>
                 <button
                   @click="doneToDo(index)"
-                  class="invisible rounded-md bg-green-500 px-2 py-1 font-thin text-white duration-200 ease-in-out hover:bg-green-600 group-hover:visible"
+                  class="invisible rounded-md bg-green-500 px-2 py-1 font-medium text-white duration-200 ease-in-out hover:bg-green-600 group-hover:visible"
                 >
                   Done
                 </button>
@@ -105,16 +110,27 @@
             </div>
           </div>
         </div>
+        <div
+          v-else
+          class="my-2 flex flex-col-reverse gap-2 rounded-lg px-2 py-6 text-center text-slate-500"
+        >
+          <p>Nothing to do</p>
+        </div>
       </div>
       <div class="w-full sm:w-1/2">
-        <h1 class="my-4 text-3xl font-medium">Done</h1>
+        <h1
+          class="my-4 border border-transparent border-b-gray-200 py-2 text-3xl font-medium"
+        >
+          Done
+        </h1>
         <div
+          v-if="checkToDoDone"
           v-for="(list, index) in toDoDone"
           :key="index"
-          class="my-2 flex flex-col gap-2 rounded-lg border p-2"
+          class="my-2 flex flex-col gap-2 rounded-lg border"
         >
-          <div class="group flex flex-col gap-2">
-            <p class="font-medium">{{ list.name }}</p>
+          <div class="group flex flex-col gap-2 p-2 hover:bg-slate-50">
+            <p class="text-2xl font-medium sm:text-base">{{ list.name }}</p>
             <div class="flex justify-between text-sm">
               <p class="flex items-center gap-1">
                 <span
@@ -127,13 +143,19 @@
               <div class="flex">
                 <button
                   @click="deleteDone(index)"
-                  class="invisible rounded-md bg-red-500 px-2 py-1 font-thin text-white duration-200 ease-in-out hover:bg-red-600 group-hover:visible"
+                  class="invisible rounded-md bg-red-500 px-2 py-1 font-medium text-white duration-200 ease-in-out hover:bg-red-600 group-hover:visible"
                 >
                   Delete
                 </button>
               </div>
             </div>
           </div>
+        </div>
+        <div
+          v-else
+          class="my-2 flex flex-col-reverse gap-2 rounded-lg px-2 py-6 text-center text-slate-500"
+        >
+          <p>Get some rest</p>
         </div>
       </div>
     </div>
@@ -183,6 +205,16 @@ export default {
     countHighPriority() {
       return this.countPriority("High");
     },
+    checkToDoList() {
+      if (this.toDoList.length !== 0) {
+        return true;
+      }
+    },
+    checkToDoDone() {
+      if (this.toDoDone.length !== 0) {
+        return true;
+      }
+    },
   },
   methods: {
     countPriority(priority) {
@@ -224,9 +256,9 @@ export default {
       }
 
       return {
-        "bg-blue-400": low,
-        "bg-orange-400": medium,
-        "bg-red-400": high,
+        "bg-blue-400 ": low,
+        "bg-orange-400 ": medium,
+        "bg-red-400 ": high,
       };
     },
   },
